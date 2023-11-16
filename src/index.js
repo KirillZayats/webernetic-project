7e2b8aa7c3870ade89833bf78f8fe597930d4a7b
Kirill <kirill.zayats.99@mail.ru>
2023-11-16 21:03:53 +0300

    feat: add animations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-import './scss/style.scss';
+import "./scss/style.scss";
+
+{
+  const isElementInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.bottom <= document.documentElement.clientHeight && rect.top >= 0
+    );
+  };
+
+  const animateScrollingElements = (name) => {
+    const element = document.querySelector(name);
+    if (isElementInViewport(element)) {
+      let index = 0;
+      for (const child of element.children) {
+        if (!child.getAttribute("class").includes("animate")) {
+          setTimeout(() => {
+            child.classList.add("animate");
+          }, (index + 1) * 100);
+          index++;
+        }
+      }
+    }
+  };
+
+  document.addEventListener("DOMContentLoaded", () => {
+    animateScrollingElements(".list-colors");
+    animateScrollingElements(".list-fonts");
+    animateScrollingElements(".container-images");
+  });
+  window.addEventListener("scroll", () => {
+    animateScrollingElements(".list-colors");
+    animateScrollingElements(".list-fonts");
+    animateScrollingElements(".container-images");
+  });
+}
